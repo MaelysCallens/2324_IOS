a7246325ab460a498b1c647639e99810bf0c098a
MaelysCallens <100351753+MaelysCallens@users.noreply.github.com>
2024-01-02 16:45:54 +0100

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Maëlys Callens on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
