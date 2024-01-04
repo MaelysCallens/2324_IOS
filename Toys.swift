27b57d50550b9e22f2a11d91a4805aabdf0b9856
MaelysCallens <100351753+MaelysCallens@users.noreply.github.com>
2024-01-04 16:50:12 +0100

    gepruts

--- Toys.swift
+++ Toys.swift
@@ -20,8 +20,10 @@ struct Toy: Codable, Identifiable {
 }
 
 class Api : ObservableObject{
+    
     @Published var toys = [Toy]()
     
+    //https://medium.com/@adrian.creteanu/link-your-xcode-project-with-github-5d1aeb582b08
     func loadData(completion:@escaping ([Toy]) -> ()) {
         guard let url = URL(string: "https://noams-toys-api-doc.cyclic.app/toys/price?min=0") else {
             print("Invalid url...")
@@ -53,6 +55,23 @@ class Api : ObservableObject{
         }.resume()
     }
     
+    func loadDataWithName(name: String, completion:@escaping ([Toy]) -> ()) {
+        guard let url = URL(string: "https://noams-toys-api-doc.cyclic.app/toys/search?s=groot\(name)") else {
+            print("Invalid url...")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            let toys = try! JSONDecoder().decode([Toy].self, from: data!)
+            print(toys)
+            DispatchQueue.main.async {
+                completion(toys)
+            }
+        }.resume()
+    }
+    
+
+    
     func LoadDataOfSingleToy(id: String, completion:@escaping (Toy) -> ()) {
         guard let url = URL(string: "https://noams-toys-api-doc.cyclic.app/toys/singleToy/\(id)") else {
             print("Invalid url...")
@@ -66,5 +85,47 @@ class Api : ObservableObject{
                 completion(toy)
             }
         }.resume()
+    }
+    
+    //https://stackoverflow.com/questions/49683960/http-request-delete-and-put
+    func DeleteToy(id: String, completion:@escaping (Bool) -> ()) {
+        guard let url = URL(string: "https://noams-toys-api-doc.cyclic.app/toys/\(id)") else {
+            print("Invalid url...")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                // als statuscode = 200 => toy is succesvol verwijderd
+                DispatchQueue.main.async {
+                    completion(true)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion(false)
+                }
+            }
+        }.resume()
+    }
+    
+    func CreateToy(completion:@escaping (Toy) -> ()) {
+        guard let url = URL(string: "https://noams-toys-api-doc.cyclic.app/toys") else {
+            print("Invalid url...")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            let toy = try! JSONDecoder().decode(Toy.self, from: data!)
+//            print(toy)
+//            DispatchQueue.main.async {
+//                completion(toy)
+//            }
+//        }.resume()
     }
 }
