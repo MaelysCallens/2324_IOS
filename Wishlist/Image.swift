b0a3c61bdfd0e2e02d1a0f787d6b5b97fb5a2165
MaelysCallens <100351753+MaelysCallens@users.noreply.github.com>
2024-01-07 16:15:55 +0100

    Opschonen code

--- Wishlist/Image.swift
+++ Wishlist/Image.swift
@@ -46,20 +46,20 @@ func getImageUrl(forCategory category: String) -> URL {
 
 //Image ophalen van API via toy.img_url
 //AsyncImage(url: URL(string: toy.img_url)) { phase in
-//                switch phase {
-//                case .success(let image):
-//                    image
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fit)
-//                        .frame(width: 60, height: 60)
-//                case .failure:
-//                    Image(systemName: "photo") // Als foto niet aanwzig is in API, dan word er deze foto geplaatst
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fit)
-//                        .frame(width: 60, height: 60)
-//                case .empty:
-//                    ProgressView() // Er wordt een laadindicator toegevoegd terwijl de afbeelding wordt geladen
-//                @unknown default:
-//                    EmptyView()
-//                }
-//            }
+//    switch phase {
+//        case .success(let image):
+//            image
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 60, height: 60)
+//        case .failure:
+//            Image(systemName: "photo") // Als foto niet aanwzig is in API, dan word er deze foto geplaatst
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 60, height: 60)
+//        case .empty:
+//            ProgressView() // Er wordt een laadindicator toegevoegd terwijl de afbeelding wordt geladen
+//        @unknown default:
+//            EmptyView()
+//    }
+//}
